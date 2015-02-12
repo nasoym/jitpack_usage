c99e882a388120e51440d560045d899e20b1c832
sinan <sinangoo@gmx.de>
2015-02-12 13:15:38 +0100

    added basic application and tests

---
+++ app/src/main/java/com/sinangoo/Main.java
@@ -0,0 +1,15 @@
+package com.sinangoo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("start main");
+    }
+
+
+}
